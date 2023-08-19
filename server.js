29350ed8b0c45d2111ea74730d43b82e3e71781a
Ghalib craft <86109290+Ghalib-craftLeProgrammeur@users.noreply.github.com>
2023-08-19 12:32:25 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -35,7 +35,33 @@ app.post('/addEpisode', (req, res) => {
         res.status(404).json({ error: 'Anime not found' });
     }
 });
+// Route to add a new anime
+app.post('/addAnime', (req, res) => {
+    const newAnime = req.body;
+    const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
+    // Check if the anime already exists
+    const existingAnime = data.find(anime => anime.title === newAnime.title);
+    if (existingAnime) {
+        res.status(400).json({ error: 'Anime already exists' });
+        return;
+    }
+
+    // Create a new anime object
+    const animeObject = {
+        title: newAnime.title,
+        totalEpisodes: newAnime.totalEpisodes,
+        // Add more properties as needed for the new anime
+        episodes: [] // Initialize episodes as an empty array
+    };
+
+    // Add the new anime object to the data array
+    data.push(animeObject);
+
+    // Write the updated data to the data.json file
+    fs.writeFileSync('data.json', JSON.stringify(data, null, 2), 'utf8');
+    res.json({ message: 'New anime added successfully' });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
