5aafbfca1f7637e06c25c608f2068a6557ce6871
Ghalib craft <86109290+Ghalib-craftLeProgrammeur@users.noreply.github.com>
2024-01-06 20:24:16 +0100

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -15,22 +15,8 @@ import {
 } from "firebase-admin/firestore";
 
 initializeApp({
-  credential: cert({
-    type: "service_account",
-    project_id: "ot-streaming",
-    private_key_id: "99d55281c800cb9df075ca4a3520706bb02d3777",
-    private_key:
-      "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDGHCE+j7fDFpnW\nrBC5ecf5YGUnWZCAVa0t7l91HVdTQZLhWrO+DWGb5EtADm+AKQfF1R9gjxTcPgAI\nqyXXDkZtj/Mtc11oC3Fg89KLn+SLZnSbbWlr1QB9q+n1/FROcynPV8asFF5LxEqI\nOQ1h6U39qWg1khQMI7VQY9GAtUp9v88SZpTnhaMkbcU/JBG1f85h68TEZIF+S4x9\nZGXglrh137co+kIpQgsznfyZ/WcfMhdr9ddDQM4QBVYxIzoPX/NlIvDZvaflwPY5\nPESzbYHgaWP2I0jPE1CieRNTz0R+IBO9rL4dPmEAWaiElVXTJrNB0g3zqGEcfpIK\nhEnl9L4VAgMBAAECggEADHOCJ2NzAkDuZE6mMnIT/nlvghyf92DrJILh3wN8FJB/\ngchomZ5PrzOXAyylF2H8yhNOCIyVnA4NhH31P+H2zTYJ66ZfjYWCl7xbVEk1XQmQ\ndKBYyt01TbC7YKl+Rvv3+b8jjnctQgfRQ2SKkcfPDg1SrmNC/UxVPfs4FKXQS7zg\n7us8ElY9fU2Hof2nY68/q8FgW9huhxlgeNxeLTlf0qp+39Pj2+pzsv6oX+7EyXP8\n3L64cF++XT6AVMw2ipMByjy3MJ/EeUw61vm/zbi3tUcOJtyOps/AzZbrOAwMyPMi\noZI0AWtP4LZeYuBgqpaAicoCwyV9rdblKaDFxXIFVQKBgQD9QovYrSaekJpT8PTS\ncR2xitCeLh9N/GNqxdCMEo5hDc7wLVjU8EZAkF51NLXionlOD4aU2F2W5pnk0xlm\nB3IVgeaMPGDCWHzexXFV7Uo898/Us4/eMtn+SG52r6OKdKvhiuqWSkcM3xhsEoxn\nH2VgdrmhYF+zAM+gpjbBjXIAkwKBgQDIQNWjtHp/2e5bpP3gJ7Elioh3nj0ZL0B5\nlXdyCbm3rQiKLdz8nfWpm5TW2Z38rGfYqrVqn5SMbwmE7SNp0/++XKXGS4M83rPE\nt3BEoy0vxw2RoIEMzSx6+ijkLeF3eSTq3f6noUmpBtNI7C0h+pjLCZXPgoyvJOh+\noOd0sUZ3twKBgQCxnF7I+T2xTEX/AmgfwZaKcYi2ti/cGFl8gvgrk8PoSq/K/nJi\n+5K5KsYjunCoXTtnRDok3sxbzKM2t9kDFcurS6tsw9cz1KzV6m/+S+iZc018pOUL\ndmwtCYz2jVzK9+Gmzi6Qccz8OOU+vY9brsgA66Jpi1XyCmqSBp82BEVkIwKBgQCo\nGjz9Ypje0KwWBIgV7lv33rY2bABwLslaB02wzlc3yCp+DIgRC+zYh0FHpoDIRlYa\nfRszuWhzBxcfng33Xb63J4EF2AgARhb0kOTOyYg6LkfGss0CWQ344h0HhzhZGboS\n625Fe9Cg8pjVA2XTu1EeSBqZisbclAgTnLEPtW0pOwKBgQDi5y3PyMx0Mb94+TKV\n1rJCfJ2lrA4VtvVX+p+AX4yFCT04VpDKO+evbctnFq0dUO2aVk+0rqUZL8vCxydZ\n0eWnavki17aUUcqGuDzULMIT3QFKLwZJ7tzk3Vfg2IZUBk5ojBL1LufRRAacXShn\nG0mPIL8N4diQkQIUwpkBl0BIxw==\n-----END PRIVATE KEY-----\n",
-    client_email:
-      "firebase-adminsdk-f507p@ot-streaming.iam.gserviceaccount.com",
-    client_id: "102326331420631354904",
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-f507p%40ot-streaming.iam.gserviceaccount.com",
-    universe_domain: "googleapis.com",
-  }),
+  credential: cert(Your firebase credentiel
+),
 });
 
 const db = getFirestore();
